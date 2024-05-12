5db2769005fb2a571ce70212bec818e50b2b6bf0
Akash Patra <akashpatra26@gmail.com>
2024-05-12 22:33:50 +0530

    Added Bottom Navigation Bar with two sections

--- app/src/main/java/com/binarybrewers/expiryguardian/MainActivity.kt
+++ app/src/main/java/com/binarybrewers/expiryguardian/MainActivity.kt
@@ -3,31 +3,154 @@ package com.binarybrewers.expiryguardian
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.binarybrewers.expiryguardian.ui.theme.ExpiryGuardianTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             ExpiryGuardianTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(navController)
+//                    Greeting("Android")
                 }
             }
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavHostController) {
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(navController)
+        },
+        content = {
+            Box(modifier = Modifier.padding(it)) {
+                Navigations(navController)
+            }
+        }
+    )
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Categories
+    )
+    var selectedItem by remember {
+        mutableStateOf(0)
+    }
+    var currentRoute by remember {
+        mutableStateOf(NavigationItem.Home.route)
+    }
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    currentRoute = item.route
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Icon(item.icon, contentDescription = item.title)
+                },
+                alwaysShowLabel = true,
+                label = {
+                    Text(item.title)
+                }
+            )
+        }
+    }
+}
+
+
+sealed class NavigationItem(var route: String, var icon: ImageVector, var title: String) {
+    object Home : NavigationItem("Home", Icons.Rounded.Home, "Home")
+    object Categories : NavigationItem("Categories", Icons.Rounded.List, "Categories")
+}
+
+@Composable
+fun Navigations(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Home.route) {
+        composable(NavigationItem.Home.route) {
+            HomeScreen()
+        }
+        composable(NavigationItem.Categories.route) {
+            CategoriesScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    CenterText(text = "Home")
+}
+
+@Composable
+fun CategoriesScreen() {
+    CenterText(text = "Categories")
+}
+
+@Composable
+fun CenterText(text: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text, fontSize = 32.sp)
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
